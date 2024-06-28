2c357517d1ce7fd8683357d8026424cf4af88769
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-06-28 17:22:49 -0400

    Delete client directory
    se elimino el client

--- client/src/app/store/cart/page.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function CartPage(){
-    return (
-        <div>
-            <h1>Cart Page</h1>
-            <p>Display cart details here!</p>
-        </div>
-    );
-}
--- client/src/config.js
+++
@@ -1,4 +0,0 @@
-export const serverUrl = "http://localhost:8000";
-export const apiUrl = `${serverUrl}/api`;
-export const imagesURL = `${serverUrl}/images`;
-export const sliderURL = `${serverUrl}/slider`;
