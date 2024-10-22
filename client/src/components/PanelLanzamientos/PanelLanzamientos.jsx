6899d59f877c26938dffd504cb10dd45965f4523
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-10-22 12:20:24 -0300

    refactor: Actualizar dependencias de React y agregar componente Carousel

--- client/src/components/PanelLanzamientos/PanelLanzamientos.jsx
+++ client/src/components/PanelLanzamientos/PanelLanzamientos.jsx
@@ -2,29 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { apiUrl } from "@/config";
 import ProductCard from "../ProductCard/ProductCard";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-const PrevArrow = (props) => (
-  <div
-    className="slick-arrow slick-prev"
-    onClick={props.onClick}
-    style={{ left: "60px", zIndex: 1 }}
-  >
-    Prev
-  </div>
-);
-
-const NextArrow = (props) => (
-  <div
-    className="slick-arrow slick-next"
-    onClick={props.onClick}
-    style={{ right: "60px", zIndex: 1 }}
-  >
-    Next
-  </div>
-);
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 export default function PanelLanzamientos() {
   const [products, setProducts] = useState([]);
@@ -41,41 +20,40 @@ export default function PanelLanzamientos() {
     getLanzamientos();
   }, []);
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-    prevArrow: <PrevArrow />,
-    nextArrow: <NextArrow />,
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 600 },
+      items: 3,
+    },
+    mobile: {
+      breakpoint: { max: 600, min: 0 },
+      items: 2,
+    },
   };
 
   return (
     <div>
       <h1 className="text-center text-xl font-bold pb-3 text-[#FFA07A]">LANZAMIENTOS</h1>
       {products.length > 0 && (
-        <Slider {...settings} lazyLoad='anticipated'>
+        <Carousel
+          responsive={responsive}
+          infinite={true}
+          arrows={true}
+          showDots={true}
+          autoPlaySpeed={500}
+          customTransition="all .5"
+          transitionDuration={500}
+          containerClass="carousel-container"
+          itemClass="carousel-item-padding-40-px"
+        >
           {products.map((product, index) => (
             <ProductCard key={index} product={product} />
           ))}
-        </Slider>
+        </Carousel>
       )}
     </div>
   );
