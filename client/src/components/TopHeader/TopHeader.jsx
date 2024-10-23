60a3265e4ab38c313807ba5af1ceae9a2ece7222
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-10-23 12:38:03 -0300

    Actualizar diseño de encabezado superior para mostrar logo y menú de navegación

--- client/src/components/TopHeader/TopHeader.jsx
+++ client/src/components/TopHeader/TopHeader.jsx
@@ -8,7 +8,6 @@ import {
   faSearchMinus,
   faSearch,
 } from "@fortawesome/free-solid-svg-icons";
-import { FaSearch, FaSearchMinus } from "react-icons/fa";
 import { useCart } from "@/contexts/CartContext";
 
 const TopHeader = () => {
@@ -30,91 +29,78 @@ const TopHeader = () => {
   };
 
   return (
-    <div className="flex flex-col items-center bg-[#F9E4B7]">
-      <div className="flex items-center p-2 px-[30px] w-full">
-        <div className="flex-1"></div>
+    <header className="bg-[#FFA07A] shadow-md sticky top-0 z-10">
+      <div className="container mx-auto flex items-center justify-between p-4">
+        {/* Contenedor del logo y navegación */}
+          {/* Logo */}
+          <div className="text-3xl font-bold text-white">
+            <Link href="/store">FRESH</Link>
+          </div>
 
-        <div className="flex-1 text-center">
-          <Link href="/store" className="text-center">
-            <h1 className="font-candal text-center ms-12 text-3xl py-4 text-[#FFA07A]">FRESH</h1>
-          </Link>
-        </div>
-
-        <div className="flex-1 flex gap-4 pe-12 items-center justify-end">
-          <button
-            type="button"
-            className="text-white"
-            onClick={() => {
-              setShowSearch(!showSearch);
-            }}
-          >
-            <FontAwesomeIcon
-              icon={!showSearch ? faSearch : faSearchMinus}
-              className={`text-white ${showSearch ? " " : ""}`}
-            />
-          </button>
-          <Link href="/store/user/login">
-            <FontAwesomeIcon
-              className="text-white"
-              icon={faUser}
-            ></FontAwesomeIcon>
-          </Link>
-          <button className="relative" onClick={goToCheckout}>
-            <span className="absolute top-[-18px] right-[-11px] rounded-full bg-white h-[16px] w-[16px] flex items-center justify-center text-sm font-bold text-black">
-              {calculateTotalCount()}
-            </span>
-            <FontAwesomeIcon
-              className="text-white"
-              icon={faCartShopping}
-            ></FontAwesomeIcon>
-          </button>
-        </div>
-      </div>
-      <form
-        onSubmit={handleSubmit}
-        className={`bg-[rgba(0,0,0,0.5)] ${
-          showSearch ? "flex" : "hidden"
-        }  absolute top-0 left-0 w-full h-full z-50   items-center justify-around`}
-      >
-        <div className="flex w-96 bg-slate-300 rounded-md">
-          <input
-            type="text"
-            placeholder="Buscar..."
-            className=" focus:outline-none w-96 rounded-s-md p-2"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <button type="submit" className="px-2 w-9 bg-slate-300">
-            <FaSearch className="text-slate-600" />
-          </button>
-        </div>
-      </form>
-      <div className="w-full flex justify-center text-[#000000] p-2">
-        <ul className="flex gap-6  text-md">
-          <Link href={"/store"}>
-            <li className=" hover:bg-[#FFA07A] hover:text-white hover:rounded-full px-2 py-1">Inicio</li>
+          {/* Menú de navegación */}
+          <nav className="ml-10 flex items-center max-lg:invisible ">
+            <ul className="flex space-x-6 text-white text-sm uppercase tracking-wide">
+            <Link href={"/store"}>
+            <li className=" hover:text-[#FFD700] transition-colors">Inicio</li>
           </Link>
           <Link  href={"/store/categories/65c963770675d5d5ad5f2082"}>
-            <li className="hover:bg-[#FFA07A] hover:text-white hover:rounded-full px-2 py-1">Tenis</li>
+            <li className="hover:text-[#FFD700] transition-colors">Tenis</li>
           </Link>
           <Link href={"/store/categories/65d1cf4a6c6ca2453aa3b56f"}>
-            <li className="hover:bg-[#FFA07A] hover:text-white hover:rounded-full px-2 py-1">Sandalias</li>
+            <li className="hover:text-[#FFD700] transition-colors">Sandalias</li>
           </Link>
           <Link href={"/store/categories/65d1cf6e6c6ca2453aa3b571"}>
-            <li className="hover:bg-[#FFA07A] hover:text-white hover:rounded-full px-2 py-1">Botas</li>
+            <li className="hover:text-[#FFD700] transition-colors">Botas</li>
           </Link>
           <Link href={"/store/categories/65d1cf776c6ca2453aa3b573"}>
-            <li className="hover:bg-[#FFA07A] hover:text-white hover:rounded-full px-2 py-1">Infantil</li>
+            <li className="hover:text-[#FFD700] transition-colors">Infantil</li>
           </Link>
           <Link href={"/store/contacto"}>
-            <li className=" hover:bg-[#FFA07A] hover:text-white hover:rounded-full px-2 py-1">Contacto</li>
+            <li className=" hover:text-[#FFD700] transition-colors">Contacto</li>
           </Link>
           <Link href={"/store/devoluciones"}>
-            <li className=" hover:bg-[#FFA07A] hover:text-white hover:rounded-full px-2 py-1">Cambios y devoluciones</li>
+            <li className=" hover:text-[#FFD700] transition-colors">Cambios y devoluciones</li>
           </Link>
-        </ul>
+            </ul>
+          </nav>
+
+        {/* Contenedor de iconos (búsqueda, usuario, carrito) */}
+        <div className="flex items-center space-x-4">
+          <button onClick={() => setShowSearch(!showSearch)} className="text-white hover:text-[#FFD700]">
+            <FontAwesomeIcon icon={!showSearch ? faSearch : faSearchMinus} />
+          </button>
+
+          <Link href="/store/user/login">
+            <FontAwesomeIcon icon={faUser} className="text-white hover:text-[#FFD700]" />
+          </Link>
+
+          <button className="relative" onClick={goToCheckout}>
+            <span className="absolute top-0 right-0 translate-x-1/2 translate-y-1/2 bg-[#FFD700] text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+              {calculateTotalCount()}
+            </span>
+            <FontAwesomeIcon icon={faCartShopping} className="text-white hover:text-[#FFD700]" />
+          </button>
+        </div>
       </div>
-    </div>
+
+      {/* Search Form */}
+      {showSearch && (
+        <form onSubmit={handleSubmit} className="flex justify-center bg-gray-200 py-2 max-lg:justify-start">
+          <div className="flex w-96 bg-white shadow-md rounded-full p-2">
+            <input
+              type="text"
+              placeholder="Buscar..."
+              className="w-full px-4 py-2 text-gray-700 bg-transparent focus:outline-none rounded-l-full"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button type="submit" className="px-4 text-gray-600 hover:text-gray-900">
+              <FontAwesomeIcon icon={faSearch} />
+            </button>
+          </div>
+        </form>
+      )}
+    </header>
   );
 };
 
